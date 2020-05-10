577b7edbb507317584e5bfbc5ead76d531d6248e
Rafal Bedzkowski <ravcik@gmail.com>
2020-05-10 21:23:01 +0200

    Unify Desktop builds, cleanup

--- cmake/DependenciesWindows.cmake
+++ cmake/DependenciesWindows.cmake
@@ -1,13 +1,14 @@
 macro(add_windows_dependencies)
     add_library(Dependencies INTERFACE)
-    list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_SOURCE_DIR}/deps/SDL")
     find_package(SDL REQUIRED)
+    set(SDL_INCLUDE_DIR "${SDL_INCLUDE_DIR}/../")
     target_link_libraries(Dependencies INTERFACE
             ${SDL_LIBRARY}
             )
 
     target_compile_definitions(Dependencies INTERFACE
             SPELUNKY_PSP_PLATFORM_WINDOWS
+            SPELUNKY_PSP_PLATFORM_DESKTOP
             )
 endmacro()
 
---
+++ scripts/setup-windows.ps1
@@ -0,0 +1,30 @@
+[CmdletBinding()]
+param (
+    [Parameter()][String]
+    $Version="1.2.15",
+
+    [Parameter()][String]
+    $DepsDirectory="deps"
+)
+Push-Location ..
+Try {
+    Invoke-WebRequest -Uri "https://www.libsdl.org/release/SDL-devel-$Version-VC.zip" -OutFile "SDL.zip"
+    
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    
+    Remove-Item "$DepsDirectory\SDL*" -Recurse -Force
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory("SDL.zip", "$DepsDirectory\")
+    
+    New-Item -Path "$DepsDirectory\SDL\include\SDL" -ItemType "Directory"
+    Move-Item -Path "$DepsDirectory\SDL-$Version\lib" -Destination "$DepsDirectory\SDL\" -Force
+    Move-Item -Path "$DepsDirectory\SDL-$Version\include\*.h" -Destination "$DepsDirectory\SDL\include\SDL\" -Force
+    Remove-Item -Path "$DepsDirectory\SDL-$Version" -Force -Recurse
+}
+Catch {
+    
+}
+Finally {
+    Remove-Item -path "SDL.zip"
+    Pop-Location
+}
